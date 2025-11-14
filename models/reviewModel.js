f0b21cf3936fafc52bf1df870baf975f5ca07f09
moorejo6 <moorejo6@oregonstate.edu>
2025-11-13 16:13:50 -1000

    changes

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,12 +1,12 @@
 import db from "../db/db.js";
 
-
+// Louie
 export const getReview = () => {};
 
-
+// Abraham
 export const createReview = () => {};
 // export const updateReview = () => {};
 
-
+// Jordan
 export const deleteReview = () => {};
 // export const getScore = () => {};
