9828d5e6a95b6e4a00850f35c9316c199a7884ce
Prismo <willclarkmusic@gmail.com>
2025-11-14 19:11:13 -0800

    cors update

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ import db from "./db/db.js"
 
 const app = express();
 
+// Configure CORS
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:8000", "http://localhost:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
